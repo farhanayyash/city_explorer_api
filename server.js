ee9afe5915b56e72f255f14e4f58961c1c9d440c
farhanayyash <farhanayyash16@yahoo.com>
2021-02-19 15:49:51 +0200

    lab07

--- server.js
+++ server.js
@@ -1,52 +1,113 @@
 'use strict';
-
 require('dotenv').config();
+
 const express = require('express');
-const app = express();
 const cors = require('cors');
+const superagent = require('superagent');
+
+
 const PORT = process.env.PORT || 3000;
-
-app.use(express.static('./public'));
-
+const app = express();
 app.use(cors());
 
-app.get('/location', (req, res) => {
-  const location_Json = require('./data/location.json');
-  const cityREQ = req.query.city;
-  let locationData = new Location(cityREQ, location_Json);
-  res.status(200).send(locationData);
 
-})
+// handle the main route
 
-app.get('/weather', (req, res) => {
-  const weather = require('./data/weather.json');
-
-  let weatherArr = [];
-  weather.data.forEach(element => {
-      let time = element.datetime;
-      let forecast = element.weather.description
-      let weatherData = new Weather(forecast, time);
-      weatherArr.push(weatherData);
-
-  });
-  res.status(200).send(weatherArr);
-  
-
+app.get('/', (request, response) => {
+    response.status(200).send('Welcome to my page for testing API');
 });
 
-function Location(cityREQ, location_Json) {
-  this.search_query = cityREQ;
-  this.formatted_query = location_Json[0].display_name;
-  this.latitude = location_Json[0].lat;
-  this.longitude = location_Json[0].lon;
+// localhost: 3030 / location ? city = lynwood 
+app.get('/location', locationHandler);
+app.get('/weather', weatherHandler);
+app.get('/park', parkHandler);
+
+app.use('*', notFoundHandler);
+app.use(errorHandler);
+
+
+
+function locationHandler(request, response) {
+    const city = request.query.city;
+    console.log(city);
+    // getLocation(city)
+    let key = process.env.LOCATIONIO_KEY;
+    let URL = `https://eu1.locationiq.com/v1/search.php?key=${key}&q=${city}&format=json`;
+    superagent.get(URL)
+        .then(geoData => {
+            const locationData = new Location(city, geoData.body);
+            response.status(200).send(locationData);
+        })
+        .catch(() => {
+            errorHandler('something went wrong in gtting the data from locationiq web', request, response)
+        })
+
 }
 
-function Weather(forecast, time) {
-  this.forecast = forecast;
-  this.time = time;
+
+
+function Location(city, geoData) {
+    this.search_query = city;
+    this.formatted_query = geoData[0].display_name;
+    this.latitude = geoData[0].lat;
+    this.longitude = geoData[0].lon;
 }
 
-app.use('*', (request, response) => response.status(500).send('Sorry, that route does not exist.'));
 
-app.listen(PORT,() => console.log(`Listening on port ${PORT}`));
+function weatherHandler(request, response) {
+    let lat = request.query.latitude;
+    let lon = request.query.longitude;
+    getWeather(lat, lon)
+        .then(val => {
+            response.status(200).json(val);
+        });
+}
 
+function getWeather(lat, lon) {
+    let weatherSummaries = [];
+    let key = process.env.WEATHER_KEY;
+    // console.log('lon= ' + lon + '>>>>>>', 'lat= ' + lat + '>>>>>>>', 'key=' + key);
+    let url = `https://api.weatherbit.io/v2.0/forecast/daily?lat=${lat}&lon=${lon}&days=8&units=S&key=${key}`;
+
+    return superagent.get(url)
+        .then(weatherData => {
+            let data = weatherData.body.data;
+            console.log(data);
+            return data;
+        })
+        .then(weatherData => {
+
+            weatherSummaries = weatherData.map(val => {
+                return new Weather(val)
+            });
+            return weatherSummaries
+        });
+}
+
+function Weather(day) {
+    this.forecast = day.weather.description;
+    this.time = new Date(day.valid_date).toString().slice(0, 15);
+}
+
+
+
+function parkHandler(request, response) {
+  response.status(404).send("parkHandler");
+}
+
+
+
+
+
+function notFoundHandler(req, res) {
+    res.status(404).send('Not Found');
+};
+
+function errorHandler(error, req, res) {
+    res.status(500).send(error);
+};
+
+
+app.listen(PORT, () => {
+    console.log(`Listening on PORT ${PORT}`);
+})
