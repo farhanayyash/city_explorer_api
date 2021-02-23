75a291bb876d4c633d11947b659fb420fd5214ed
farhanayyash <farhanayyash16@yahoo.com>
2021-02-23 22:05:38 +0200

    lab08

--- server.js
+++ server.js
@@ -3,12 +3,18 @@ require('dotenv').config();
 
 const express = require('express');
 const cors = require('cors');
+const pg = require('pg');
 const superagent = require('superagent');
 
 
 const PORT = process.env.PORT || 3000;
+// const client = new pg.Client(process.env.DATABASE_URL);
+const client = new pg.Client({ connectionString: process.env.DATABASE_URL,   ssl: { rejectUnauthorized: false } });
+
 const app = express();
 app.use(cors());
+
+
 
 
 // handle the main route
@@ -17,35 +23,69 @@ app.get('/', (request, response) => {
     response.status(200).send('Welcome to my page for testing API');
 });
 
-// localhost: 3030 / location ? city = lynwood 
-app.get('/location', locationHandler);
+app.get('/location', locationHandling);
 app.get('/weather', weatherHandler);
 app.get('/parks', parkHandler);
+
+
+
 
 app.use('*', notFoundHandler);
 app.use(errorHandler);
 
 
+function locationHandling(req, res) {
+    const cityData = req.query.city;
+    let locationAPIKey = process.env.GEOCODE_API_KEY;
+    const url = `https://eu1.locationiq.com/v1/search.php?key=${locationAPIKey}&q=${cityData}&format=json`;
 
-function locationHandler(request, response) {
-    const city = request.query.city;
-    // console.log(city);
-    // getLocation(city)
-    let key = process.env.LOCATIONIO_KEY;
-
-    let URL = `https://eu1.locationiq.com/v1/search.php?key=${key}&q=${city}&format=json`;
-    superagent.get(URL)
-        .then(geoData => {
-            const locationData = new Location(city, geoData.body);
-            response.status(200).send(locationData);
-        })
-        .catch(() => {
-            errorHandler('something went wrong in gtting the data from locationiq web', request, response)
-        })
-
+    let selectAllSQL = `SELECT * FROM locations;`;
+    let selectSQL = `SELECT * FROM locations WHERE search_query=$1;`;
+    let safeValues = [];
+    client.query(selectAllSQL).then((result) => {
+        if (result.rows.length <= 0) {
+            superagent.get(url).then((data) => {
+                console.log(`from API`);
+                const locationData = new Location( cityData ,data.body);
+                insertLocationInDB(locationData);
+                console.log(locationData);
+                res.status(200).josn(locationData);
+            });
+        } else {
+            safeValues = [cityData];
+            client.query(selectSQL, safeValues).then((result) => {
+                if (result.rows.length <= 0) {
+                    superagent.get(url).then((data1) => {
+                        console.log(`From API Again`);
+                        const locationData = new Location( cityData ,data1.body);
+                        
+                        insertLocationInDB(locationData);
+                        // console.log(locationData);
+                        res.status(200).json(locationData);
+                    });
+                } else {
+                    console.log('form data base');
+                    res.status(200).json(result.rows[0]);
+                }
+            });
+        }
+    });
 }
 
 
+
+function insertLocationInDB(obj) {
+    let insertSQL = `INSERT INTO locations (search_query,formatted_query,latitude,longitude) VALUES ($1,$2,$3,$4);`;
+    let safeValues = [
+        obj.search_query,
+        obj.formatted_query,
+        obj.latitude,
+        obj.longitude,
+    ];
+    client.query(insertSQL, safeValues).then(() => {
+        console.log('storing data in database');
+    });
+}
 
 function Location(city, geoData) {
     this.search_query = city;
@@ -131,6 +171,14 @@ function park(geoData) {
 
 
 
+
+// function tryskHandler(request, response) {
+//     console.log("hii");
+//     response.status(200).send('HI');
+// }
+
+
+
 function notFoundHandler(req, res) {
     res.status(404).send('Not Found');
 };
@@ -140,6 +188,11 @@ function errorHandler(error, req, res) {
 };
 
 
-app.listen(PORT, () => {
-    console.log(`Listening on PORT ${PORT}`);
+
+
+client.connect()
+.then(()=>{
+    app.listen(PORT, () =>
+    console.log(`listening on ${PORT}`)
+    );
 })
